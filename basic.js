89f0ff6a277dc1d8a8ec28124af8ab4361496612
imtiazuddin4616 <138401314+imtiazuddin4616@users.noreply.github.com>
2023-09-11 18:15:20 +0600

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,25 @@
+
+let mark =90;
+let grade;
+
+if(mark>=80)
+{
+  grade="A+";
+}
+else if(mark>=70){
+  grade="A"
+
+}
+else if(mark>=60){
+  grade="A-"
+}
+else if(mark>=50){
+  grade="b"
+}
+else if(mark>=30){
+  grade="c"
+}
+else{
+  grade="F"
+}
+console.log(grade)
